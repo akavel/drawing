8ac236bd6ae28ab4995ead419e0828d4f4e3c79e
Mateusz Czapliński <czapkofan@gmail.com>
2016-04-09 20:04:04 +0200

    make the form maximized

--- drawing/Program.fs
+++ drawing/Program.fs
@@ -1,9 +1,8 @@
-﻿// Based on https://www.youtube.com/watch?v=3PlRyug05hM
-
-open System
+﻿open System
 open System.Windows.Forms
 
-let form = new Form(Visible=true, Text="Drawing App")
+// TODO: in future, make fullscreen: FormBorderStyle=FormBorderStyle.None; but find out a way to revert this
+let form = new Form(Visible=true, Text="Drawing App", WindowState=FormWindowState.Maximized)
 let label = new Label(Text="sample label")
 form.Controls.Add(label)
 

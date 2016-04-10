02e56cf1b28768e58259083bf524f6c0d9904f81
Mateusz Czapliński <czapkofan@gmail.com>
2016-04-10 16:12:03 +0200

    test of a new canvas component

--- drawing/Program.fs
+++ drawing/Program.fs
@@ -1,10 +1,25 @@
 ﻿open System
+open System.Drawing
 open System.Windows.Forms
 
 // TODO: in future, make fullscreen: FormBorderStyle=FormBorderStyle.None; but find out a way to revert this
 let form = new Form(Visible=true, Text="Drawing App", WindowState=FormWindowState.Maximized)
-let label = new Label(Text="sample label")
-form.Controls.Add(label)
+
+type Canvas() =
+    inherit Control()
+    override c.OnPaint(e:PaintEventArgs) =
+        //System.Diagnostics.Debug.WriteLine("OnPaint")
+        base.OnPaint(e)
+        let g = e.Graphics
+        g.DrawLine(Pens.Blue, 0, 0, c.Width, c.Height)
+        let rect = new Rectangle(100,100,200,200)
+        g.DrawEllipse(Pens.Black, rect)
+        g.DrawRectangle(Pens.Red, rect)
+
+let canvas = new Canvas()
+form.Controls.Add(canvas)
+form.SizeChanged.Add(fun e -> canvas.Refresh())
+canvas.Dock <- DockStyle.Fill
 
 [<STAThread>]
 Application.Run(form)

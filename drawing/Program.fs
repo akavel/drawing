25d567402b80e6425ec40eb3bb91db4e843ebb26
Mateusz Czapliński <czapkofan@gmail.com>
2016-04-11 00:22:40 +0200

    TODOs + MouseClick test

--- drawing/Program.fs
+++ drawing/Program.fs
@@ -2,6 +2,9 @@
 open System.Drawing
 open System.Windows.Forms
 
+// TODO: right-click or finger-click opens a menu with 3 sliders for choosing color in hcl space
+// TODO: the tool-window stays on top, can be easily closed, and also has color picker
+// TODO: add F# REPL
 // TODO: in future, make fullscreen: FormBorderStyle=FormBorderStyle.None; but find out a way to revert this
 let form = new Form(Visible=true, Text="Drawing App", WindowState=FormWindowState.Maximized)
 
@@ -17,6 +20,9 @@ type Canvas() =
         let rect = new Rectangle(100,100,200,200)
         g.DrawEllipse(Pens.Black, rect)
         g.DrawRectangle(Pens.Red, rect)
+    
+    override c.OnMouseClick(e:MouseEventArgs) =
+        System.Diagnostics.Debug.WriteLine(sprintf "btn=%A, clicks=%A" e.Button e.Clicks)
 
 
 let canvas = new Canvas(Dock=DockStyle.Fill)

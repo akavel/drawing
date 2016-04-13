851df65adce647740640093b0964824f8963e578
Mateusz Czapliński <czapkofan@gmail.com>
2016-04-13 23:04:30 +0200

    fancier CIELCH->RGB conversion with active patterns

--- drawing/Program.fs
+++ drawing/Program.fs
@@ -43,16 +43,12 @@ canvas.MouseClick.Add(fun e -> toolbox.Visible <- not toolbox.Visible)
 // via https://bl.ocks.org/mbostock/3014589
 // via https://bl.ocks.org/mbostock/3e115519a1b495e0bd95
 // + also based on https://github.com/d3/d3-color/blob/1bd8cce7f7bf18eefdbadcbe78774bf0e07a2b39/src/lab.js
-type Color = 
-    | CIELCH of L:float * C:float * H:float  // H=[0,360] C=[0,100] L=[0,150] // TODO: verify if ok
-    | CIELab of L:float * a:float * b:float
-    | XYZ of X:float * Y:float * Z:float
-    | RGB of R:float * G:float * B:float
-let lchToLab (CIELCH (l, c, h)) =
+type CIELCH = CIELCH of L:float * C:float * H:float  // H=[0,360] C=[0,100] L=[0,150] // TODO: verify if ok
+let (|CIELab|) (CIELCH (l,c,h)) =
     let deg2rad = System.Math.PI/180.0
     let hRad = h * deg2rad
-    CIELab (l, cos(hRad)*c, sin(hRad)*c)
-let labToXyz (CIELab (L, a, b)) =
+    (l, cos(hRad)*c, sin(hRad)*c)
+let (|XYZ|) (CIELab (L, a, b)) =
     let t0 = 4./29.
     let t1 = 6./29.
     let t2 = 3.*t1*t1
@@ -65,15 +61,15 @@ let labToXyz (CIELab (L, a, b)) =
     let xfact = 95.047
     let yfact = 100.000
     let zfact = 108.883
-    XYZ (convert xfact tmpx, convert yfact tmpy, convert zfact tmpz)
-let xyz2rgb (XYZ (x, y, z)) =
+    (convert xfact tmpx, convert yfact tmpy, convert zfact tmpz)
+let (|RGB|) (XYZ (x, y, z)) =
     let conv1 fx fy fz = (fx*x + fy*y + fz*z)*0.01
     let conv2 n = if n > 0.0031308
                   then 1.055 * n**(1./2.4) - 0.055
                   else 12.92 * n
-    RGB (conv1 324.06 -153.72 -49.86 |> conv2,
-         conv1 -96.89  187.58   4.15 |> conv2,
-         conv1   5.57  -20.40 105.70 |> conv2)
+    (conv1 324.06 -153.72 -49.86 |> conv2,
+     conv1 -96.89  187.58   4.15 |> conv2,
+     conv1   5.57  -20.40 105.70 |> conv2)
 
 let toolboxLayout = new TableLayoutPanel(Dock=DockStyle.Fill)
 toolbox.Controls.Add(toolboxLayout)
@@ -92,7 +88,7 @@ let recolor _ =
     let s (s:TrackBar) = float s.Value / float s.Maximum
     let lch = CIELCH (150.*s s3, 100.*s s2, 360.*s s1)
     //System.Diagnostics.Debug.WriteLine(sprintf "%A" lch)
-    let (RGB (r, g, b)) = lch |> lchToLab |> labToXyz |> xyz2rgb
+    let (RGB (r, g, b)) = lch
     //System.Diagnostics.Debug.WriteLine(sprintf "rgb %A %A %A" r g b)
     // FIXME: ad-hoc clipping to [0..255] by MC; is this OK?
     let i f = f*255.0 |> int |> max 0 |> min 255

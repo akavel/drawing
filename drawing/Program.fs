5b208e1523188cab207a33ad309a1bb3548e31cc
Mateusz Czapliński <czapkofan@gmail.com>
2016-04-14 01:16:11 +0200

    small refactoring

--- drawing/Program.fs
+++ drawing/Program.fs
@@ -82,21 +82,20 @@ let (|XYZ|) (CIELab (L, a, b)) =
     let t0 = 4./29.
     let t1 = 6./29.
     let t2 = 3.*t1*t1
-    let convert factor n = (if n > t1
-                            then n*n*n
-                            else t2*(n-t0)) * factor * 0.01
+    let convert n = 
+        if n > t1
+        then n*n*n
+        else t2*(n-t0)
     let tmpy = (L+16.)/116.
     let tmpx = tmpy + a/500.
     let tmpz = tmpy - b/200.
-    let xfact = 95.047
-    let yfact = 100.000
-    let zfact = 108.883
-    (convert xfact tmpx, convert yfact tmpy, convert zfact tmpz)
+    (0.95047 * convert tmpx, 1. * convert tmpy, 1.08883 * convert tmpz)
 let (|RGB|) (XYZ (x, y, z)) =
     let conv1 fx fy fz = (fx*x + fy*y + fz*z)*0.01
-    let conv2 n = if n > 0.0031308
-                  then 1.055 * n**(1./2.4) - 0.055
-                  else 12.92 * n
+    let conv2 n = 
+        if n > 0.0031308
+        then 1.055 * n**(1./2.4) - 0.055
+        else 12.92 * n
     (conv1 324.06 -153.72 -49.86 |> conv2,
      conv1 -96.89  187.58   4.15 |> conv2,
      conv1   5.57  -20.40 105.70 |> conv2)
